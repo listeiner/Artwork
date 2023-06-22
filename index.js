e4a13a631ddb2c06fdced8fb3ab7397024d677de
Lindsey Steiner <105761115+Razetto@users.noreply.github.com>
2023-06-22 08:30:59 -0400

    Added favicon and changed navbar

---
+++ index.js
@@ -0,0 +1,6 @@
+function toggleNavbar() {
+    var navbarMenu = document.querySelector('.navbar-menu');
+    var navbarToggle = document.querySelector('.navbar-toggle');
+    navbarMenu.classList.toggle('change');
+    navbarToggle.classList.toggle('change');
+  }

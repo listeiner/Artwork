9602168dd7b3f07a76d122101b0508c6ac52e6da
Lindsey Steiner <105761115+Razetto@users.noreply.github.com>
2023-06-22 09:51:45 -0400

    updated navbar

--- index.js
+++
@@ -1,6 +0,0 @@
-function toggleNavbar() {
-    var navbarMenu = document.querySelector('.navbar-menu');
-    var navbarToggle = document.querySelector('.navbar-toggle');
-    navbarMenu.classList.toggle('change');
-    navbarToggle.classList.toggle('change');
-  }
